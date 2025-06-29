3d48121999ca8c6986a4386fee23be676a628e31
Densodim <25denso@gmail.com>
2025-06-29 15:13:26 +0300

    work with home page

--- src/features/user/UserPage.tsx
+++ src/features/user/UserPage.tsx
@@ -1,11 +1,29 @@
-import { Outlet } from "react-router"
-import SurveyComponent from "../survey/SurveyComponent.tsx"
+import { Button, Grid, Paper, Typography } from "@mui/material"
+import { Outlet, useNavigate } from "react-router"
 
 export default function UserPage() {
+  const navigate = useNavigate()
   return (
     <>
       <Outlet />
-      <SurveyComponent />
+      <Grid container spacing={4}>
+        <Grid>
+          <Paper elevation={6} sx={{ p: 6 }}>
+            <Typography variant="h6">Title</Typography>
+            <Typography variant="body2" color="text.secondary">
+              Description
+            </Typography>
+            <Button
+              fullWidth
+              variant="outlined"
+              sx={{ mt: 2 }}
+              onClick={() => navigate(`/form/${1}`)}
+            >
+              Open
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   )
 }
