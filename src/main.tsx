a4d34529a7d16d102bf9c45c6aec8cc7fe84359e
Densodim <25denso@gmail.com>
2025-07-05 00:03:40 +0300

    add to project FilePond

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import 'survey-core/survey-core.css'
+import "survey-core/survey-core.css"
 import "survey-creator-core/survey-creator-core.css"
+import "filepond/dist/filepond.min.css"
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation"
+import FilePondPluginImagePreview from "filepond-plugin-image-preview"
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
 import { RouterProvider } from "react-router"
 import { router } from "./router/router.ts"
+import { registerPlugin } from "filepond"
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileValidateType)
 
 const container = document.getElementById("root")
 
